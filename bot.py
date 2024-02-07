5b690aaddb6d91f5e5ddbc22c96be04b3affa6b6
Godot99 <oscarhidalgonocole@mail.com>
2024-02-07 21:42:53 +0100

    tabla del 7

--- bot.py
+++ bot.py
@@ -12,6 +12,12 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+def taboa_do_7():
+    texto = ''
+    for i in range(11):
+        texto = texto + '7 x '+ str(i) + ' = ' + str(7*i) + '\n'
+    return texto
+
 # This function responds to start command handler
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Son un bot, dime algo!")
@@ -19,6 +25,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # This function responds to echo handler
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+async def table7(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=taboa_do_7()) 
 
 if __name__ == '__main__':
     # Start the application to operate the bot
@@ -31,6 +40,9 @@ if __name__ == '__main__':
     # Handler to manage text messages
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     application.add_handler(echo_handler)
+
+    table7_handler = CommandHandler('table7', table7)
+    application.add_handler(table7_handler)
 
     # Keeps the application running
     application.run_polling()
